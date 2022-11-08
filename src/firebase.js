69e877ee472e06bc532ff64eea9c4ba38d7f41cf
janeanekim <jmk90020@gmail.com>
2022-11-08 08:19:42 -0800

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCHmyQAkzECZru9pXCJAiHKjc-fMN0M2jw",
-    authDomain: "fir-playground-f0019.firebaseapp.com",
-    databaseURL: "https://fir-playground-f0019-default-rtdb.firebaseio.com",
-    projectId: "fir-playground-f0019",
-    storageBucket: "fir-playground-f0019.appspot.com",
-    messagingSenderId: "69849678247",
-    appId: "1:69849678247:web:662603f22e42ca4a17f682"
+// TODO: use CDN from own firebase config
   };
 
 
